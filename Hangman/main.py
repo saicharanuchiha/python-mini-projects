eb74ec7f49c1d9a5cbb936aa47a80a5bbf214b28
saicharanuchiha <saicherrin007@gmail.com>
2026-04-03 21:25:14 +0530

    feat: build modular Hangman CLI game
    - Separated ASCII art and word lists into dedicated modules for clean architecture.
    - Implemented state tracking for user lives, win/loss conditions, and previously guessed letters.
    - Handled dynamic string building to display correctly guessed letters and remaining blanks.

---
+++ Hangman/main.py
@@ -0,0 +1,53 @@
+import random
+import hangman_words
+import hangman_art
+
+print(hangman_art.logo)
+
+lives = 6
+
+chosen_word = random.choice(hangman_words.word_list)
+#print(chosen_word) answer
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print(f"You've already guessed a {guess}")
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"you guessed {guess}, that's not in the word. you lose a life.")
+
+        if lives == 0:
+            game_over = True
+            print(f"***********************IT WAS {chosen_word} !YOU LOSE**********************")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(hangman_art.stages[lives])
