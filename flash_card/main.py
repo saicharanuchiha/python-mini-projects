b92c596337fe83fa44223d7c982539aa9e76bb40
saicharanuchiha <saicherrin007@gmail.com>
2026-08-14 19:31:53 +0530

    refactor: adjusted duplicate cancel call.

--- flash_card/main.py
+++ flash_card/main.py
@@ -5,6 +5,7 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 french_dict = {}
+flip_timer = None
 
 try:
     data = pandas.read_csv("data/words_to_learn.csv")
@@ -12,13 +13,14 @@ except FileNotFoundError:
     original_data = pandas.read_csv("data/french_words.csv")
     french_dict = original_data.to_dict(orient="records")
 else:
-    french_dict = pandas.DataFrame(data).to_dict(orient="records")
+    french_dict = data.to_dict(orient="records")
 
 #----------------------Create Flash Cards-----------------
 def next_card():
     global current_card, flip_timer
+    if flip_timer:
+        window.after_cancel(flip_timer)
     current_card = random.choice(french_dict)
-    window.after_cancel(flip_timer)
     canvas.itemconfig(card_title, text="French", fill= "black")
     canvas.itemconfig(card_word, text= f"{current_card["French"]}", fill= "black")
     canvas.itemconfig(card_background, image= card_front_image)
@@ -40,14 +42,13 @@ def is_known():
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
-flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526,background= BACKGROUND_COLOR, highlightthickness=0)
 card_front_image = PhotoImage(file="images/card_front.png")
 card_back_image = PhotoImage(file="images/card_back.png")
 card_background =canvas.create_image(400, 270, image= card_front_image)
-card_title = canvas.create_text(400, 150, text= "", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text= "", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text= "", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text= "", font=("Arial", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_image = PhotoImage(file="images/right.png")
