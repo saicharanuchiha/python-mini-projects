69b94efc7fe31a322f97598d973207541d4411a2
saicharanuchiha <saicherrin007@gmail.com>
2026-08-21 16:57:54 +0530

    refactor(rain-alert): clean up debug code

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -21,7 +21,6 @@ parameters = {
 response = requests.get(OWM_Endpoint, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
-#print(weather_data["list"][0]["weather"][0]["id"])
 
 will_rain = False
 for hour_data in weather_data["list"]:
